2a14560d198d3f6de811d94c8cd6fe95ac70e107
Victor <victorx142010@gmail.com>
2020-12-10 19:51:46 -0500

    Cambio en gestor reporte viajes x mes

--- Carpool/CarpoolController/GestorReporteViajesxMes.cpp
+++ Carpool/CarpoolController/GestorReporteViajesxMes.cpp
@@ -88,11 +88,13 @@ void GestorReporteViajesxMes::GenerarReporte(int CodigoUsuario) {
 
 						contadorViajes = 1;
 					}
+				ViajesxMes^ objViajexMes = gcnew ViajesxMes(mes_anterior, contadorViajes);
+				this->lista->Add(objViajexMes);
 				}
+			
 			}
 			
-	//	ViajesxMes^ objViajexMes = gcnew ViajesxMes(mes_anterior, contadorViajes);
-	//	this->lista->Add(objViajexMes);
+
 		}
 		
 	}
